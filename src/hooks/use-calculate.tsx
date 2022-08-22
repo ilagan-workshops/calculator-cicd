4d3625b8e4516d439ae8eef8729819b82202d066
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2022-08-22 17:42:23 +0800

    refactor: adjusts arithmetic to support big numbers

--- src/hooks/use-calculate.tsx
+++ src/hooks/use-calculate.tsx
@@ -1,25 +1,24 @@
 import { CalculatorOperation } from '@/models/operations'
+import Big from 'big.js'
 
 // :: ---
 
 const _calculate = (operand1: string, operand2: string, operation: CalculatorOperation) => {
-	console.debug('[calculate]', operand1, operand2, operation)
-
-	const _o1 = Number(operand1)
-	const _o2 = Number(operand2)
+	const _o1 = Big(operand1)
+	const _o2 = Big(operand2)
 
 	switch (operation) {
 		case CalculatorOperation.ADD:
-			return String(_o1 + _o2)
+			return _o1.add(_o2).toString()
 
 		case CalculatorOperation.SUBTRACT:
-			return String(_o1 - _o2)
+			return _o1.sub(_o2).toString()
 
 		case CalculatorOperation.MULTIPLY:
-			return String(_o1 * _o2)
+			return _o1.mul(_o2).toString()
 
 		case CalculatorOperation.DIVIDE:
-			return String(_o1 / _o2)
+			return _o1.div(_o2).toString()
 	}
 }
 
